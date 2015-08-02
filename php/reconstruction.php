468e42fc96a4f837ed8ae2ffe882c50d1a455870
Bingo <bingo6@bupt.edu.cn>
2015-08-02 15:59:47 +0800

    fixed control

--- php/reconstruction.php
+++ php/reconstruction.php
@@ -1,14 +1,15 @@
 <?php
-echo "hello <br/>";
 $peak_threshold = $_GET["peak_threshold"];
-echo "peak_threshold=".$peak_threshold."<br/>";
+echo "peak_threshold=".$peak_threshold."\n";
 
 $myfile = fopen("upload/ok", "w") or die("Unable to open file!");
 fwrite($myfile, $peak_threshold);
 fclose($myfile);
 
-$result = system("ls upload/", $out);
-print_r($out."<br/>");
+echo "fileok";
+
+#$result = system("ls upload/", $out);
+#print_r($out."<br/>");
 
 # $result2 = system("cd upload; /home/bingo/Bundler-master/RunSFM2.sh $peak_threshold", $out);
 # print_r($out."<br/>");
